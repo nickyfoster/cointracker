d15255fb08c0b8bda961cf346c29e2f81a93fd7c
Nikita Gulyaev <n.gulyaev@just-ai.com>
2022-11-23 19:14:01 -0300

    CD updated

--- test/test_cointracker_api/test_cointracker_api.py
+++ test/test_cointracker_api/test_cointracker_api.py
@@ -1,11 +1,10 @@
 import unittest
 
-from tracker.services.Cointracker import Cointracker
 from tracker.services import CustomException
+from tracker.services.Cointracker import Cointracker
 
 
 class TestCointrackerAPI(unittest.TestCase):
-
     def test_001_invalid_coin_data(self):
         with self.assertRaises(CustomException):
             tracker = Cointracker()
